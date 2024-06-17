de30544142704d4276398e0630d8c626a2ac79ba
manishshahi64 <manishshahi128@gmail.com>
2024-06-17 19:46:40 +0530

    squareroot

---
+++ day4/69_Sqrt_of_x.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+        while left <= right:
+            mid = (left + right)//2
+            if mid * mid < x:
+                left = mid + 1
+            elif mid * mid > x:
+                right = mid - 1
+            else :
+                return mid
+        return right
+x = 8
+p1 = Solution()
+print(p1.mySqrt(x))
