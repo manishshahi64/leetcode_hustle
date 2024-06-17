dbb2b3ae15ccbf6c7772370729a4c72f5aca4692
manishshahi64 <manishshahi128@gmail.com>
2024-06-17 18:35:21 +0530

    solve 67

--- day4/67_Add_Binary.py
+++ day4/67_Add_Binary.py
@@ -7,3 +7,25 @@
 # The carry gets added in next step(scanning from right to left).
 ####
 
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        s = []
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+        while i>=0 or j>=0 or carry:
+            if i>=0:
+                carry += int(a[i])
+                i-=1
+            if j>=0:
+                carry += int(b[j])
+                j-=1
+            s.append(str(carry%2))
+            carry//=2
+        return "".join(reversed(s))
+
+
+a = "1011"
+b = "1101"
+p1 = Solution()
+print(p1.addBinary(a,b))
